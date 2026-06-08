86f9b7b4a984e36ae8248910fa44093c75b2f646
siminiceanurazvan-cloud <siminiceanurazvan@gmail.com>
2026-06-09 00:28:34 +0300

    lab12

---
+++ src/lab12/ro/ulbs/proiectaresoftware/lab12/ApplicationLab12.java
@@ -0,0 +1,29 @@
+package lab12.ro.ulbs.proiectaresoftware.lab12;
+
+import java.util.Arrays;
+
+public class ApplicationLab12 {
+
+    public static void main(String[] arg) {
+        CookingTasks t1 = new CookingTasks( Arrays.asList("Add pasta to water","boil", "add salt", "make sauce", "add sauce", "fill platter", "DONE Pasta") );
+        CookingTasks t2 = new CookingTasks(Arrays.asList("Grab salad", "grab cucumbers", "grab eggs", "boil eggs", "add eggs", "add salt", "add sauce", "DONE Salad"));
+        CookingTasks t3 = new CookingTasks(Arrays.asList("Grab flour","grab eggs","grab sugar","grab cinamon","cook all","add cream","add icecream","DONE Dessert"));
+        CookingTasks t4 = new CookingTasks(Arrays.asList("Grab rice and boil","DONE Rice"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Restaurant used water: "+Restaurant.getRestaurant().getUsedWater());
+    }
+}
---
+++ src/lab12/ro/ulbs/proiectaresoftware/lab12/Restaurant.java
@@ -0,0 +1,35 @@
+package lab12.ro.ulbs.proiectaresoftware.lab12;
+
+public class Restaurant {
+
+    private static final Restaurant restaurant;
+    private  long usedWater;
+
+    static {
+        restaurant = new Restaurant();
+    }
+
+    private Restaurant() {
+        this.usedWater = 0;
+    }
+
+    public static Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public synchronized void washHands() {
+        for(int i = 0; i < 1000; i++) {
+            this.usedWater++;
+        }
+    }
+
+    public synchronized void washDishes() {
+        for(int i = 0; i < 1000; i++) {
+            this.usedWater++;
+        }
+    }
+
+    public long getUsedWater() {
+        return usedWater;
+    }
+}
